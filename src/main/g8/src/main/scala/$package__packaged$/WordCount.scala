6c725c45764d8eaa2ab461209636d031e4ac6e34
Ikenna Darlington Ogbajie <iogbajie@gmail.com>
2018-10-11 16:39:16 +0200

    Fix formatting

--- src/main/g8/src/main/scala/$package__packaged$/WordCount.scala
+++ src/main/g8/src/main/scala/$package__packaged$/WordCount.scala
@@ -11,25 +11,27 @@ import org.apache.kafka.streams.scala.StreamsBuilder
 import org.apache.kafka.streams.scala.kstream.{ KStream, KTable }
 import org.apache.logging.log4j.scala.Logging
 
+/**
+  * Before running this application,
+  * start your kafka broker(s) and create the required topics
+  *
+  * kafka-topics --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic streams-plaintext-input
+  * kafka-topics --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic streams-wordcount-output
+  *
+  */
 object WordCount extends App with Logging {
 
   val config = new Properties()
-  config.put(
-    StreamsConfig.BOOTSTRAP_SERVERS_CONFIG,
-    "localhost:9092"
-  )
-  config.put(
-    ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,
-    "earliest"
-  )
-  config.put(
-    StreamsConfig.APPLICATION_ID_CONFIG,
-    "streams-wordcount"
-  )
+
+  // setting offset reset to earliest so that we can re-run the app with same data
+  config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
+  config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+  config.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-wordcount")
 
   val builder = new StreamsBuilder()
 
-  val textLines: KStream[String, String] = builder.stream[String, String]("streams-plaintext-input")
+  val textLines: KStream[String, String] =
+    builder.stream[String, String]("streams-plaintext-input")
 
   val wordCount: KTable[String, Long] = textLines
     .flatMapValues(words => words.split("\\\\W+"))
